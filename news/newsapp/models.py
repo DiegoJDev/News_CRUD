75d5038c14655eabb07fe148d8831b4790afe7cd
DiegoJ0 <diegocardenas.developer@gmail.com>
2022-07-11 22:56:12 -0500

    agregue el metodo __str__

--- news/newsapp/models.py
+++ news/newsapp/models.py
@@ -7,5 +7,8 @@ class News(models.Model):
     #el id se pone automaticamente gracias a django
     news_text = models.TextField #comprobar si se pueden usar mas de 250 letras o caraceres poeque si no se debe añadir el max lenght
     pub_date = models.DateField(default=timezone.now)
-    author=models.CharField
+    author = models.CharField
+
+    def __str__(self):
+        return self.news_text
 
