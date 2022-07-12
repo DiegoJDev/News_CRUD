51c2eb8c4ce123f1ec85268e14951103b5a06eda
DiegoJ0 <diegocardenas.developer@gmail.com>
2022-07-11 21:54:03 -0500

    se creo el modelo news en newsapp/models.py y se hizo el comando makemigrations, por ultimo migrate

--- news/newsapp/models.py
+++ news/newsapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class News(models.Model):
+    #el id se pone automaticamente gracias a django
+    news_text = models.TextField #comprobar si se pueden usar mas de 250 letras o caraceres poeque si no se debe añadir el max lenght
+    pub_date = models.DateField(default=timezone.now)
+    author=models.CharField
+
